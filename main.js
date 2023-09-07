15a56805b3ac64aaa9130b83f3e13db58c2ed09b
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2023-09-06 19:40:40 -0500

    Added a prompt to get the size

--- main.js
+++ main.js
@@ -13,4 +13,16 @@ function createBoard(size) {
   }
 }
 
-createBoard(16);
+function getSize() {
+  let input = prompt("What will be the size of the board?");
+  let message = document.querySelector("#message");
+  if (input === "") {
+    message.innerHTML = "Please provide a number";
+  } else if (input < 0 || input > 100) {
+    message.innerHTML = "Provide a number between 1 and 100";
+  } else {
+    message.innerHTML = "Now we play!";
+  }
+}
+
+getSize();
