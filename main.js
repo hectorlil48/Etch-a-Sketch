96e2ddbab8aa75739838603208c9f8797f5c2c3a
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2023-09-06 19:18:34 -0500

    Created a function that makes the divs

---
+++ main.js
@@ -0,0 +1,8 @@
+function createBoard(size) {
+  let board = document.querySelector(".board");
+
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+}
+
+createBoard(26);
