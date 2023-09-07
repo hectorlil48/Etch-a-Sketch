4efc3212bf1e3e2519ff51f574686babf7571393
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2023-09-06 19:28:45 -0500

    create divs function

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ function createBoard(size) {
 
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let numDivs = size * size;
+
+  for (let i = 0; i < numDivs; i++) {
+    let div = document.createElement("div");
+    div.style.backgroundColor = "yellow";
+    board.insertAdjacentElement("beforeend", div);
+  }
 }
 
-createBoard(26);
+createBoard(16);
